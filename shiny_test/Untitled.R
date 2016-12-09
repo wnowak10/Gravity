637368a7ed771c15d9743cc0a648128c3955dfc1
wnowak <wnowak10@gmail.com>
2016-12-09 15:35:49 -0500

    added shiny?

--- shiny_test/Untitled.R
+++
@@ -1,9 +0,0 @@
-runApp("/Users/wnowak/opportunity/shiny_test")
-
-library(maps)
-library(mapproj)
-source("/Users/wnowak/opportunity/shiny_test/data/helpers.R")
-counties <- readRDS("/Users/wnowak/opportunity/shiny_test/data/counties.rds")
-percent_map(counties$white, "darkgreen", "% White")
-
-map("county")
